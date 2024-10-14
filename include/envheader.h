38c9ab190818946b6decca4bf1caf14b49921e7d
motty <motty.mio2@gmail.com>
2024-10-14 12:34:09 +0900

    add esp32s3 support

--- include/envheader.h
+++ include/envheader.h
@@ -1,12 +1,14 @@
-#ifndef ENVHEADER_
-#define ENVHEADER_
+#ifndef ENVHEADER
+#define ENVHEADER
+
+#ifdef BOARD_ESP32
 
 // I2C
 #define I2C_SDA 21
 #define I2C_SCL 22
 
 // SPI
-#define SPI_DIN 32
+#define SPI_MOSI 32
 #define SPI_SCK 33
 #define SPI_CS 25
 #define SPI_DC 26
@@ -14,7 +16,7 @@
 #define SPI_BUSY 18
 
 // Current Sensor
-#define SENSE_CURRENT 23
+#define SENSE_CURRENT1 23
 
 // BME280
 #define ADDR_BME280_0 0x76
@@ -34,5 +36,37 @@
 #define LED_PIN_1 4
 #define LED_PIN_2 5
 #define LED_PIN_3 19
+#endif  // BOARD_ESP32
 
-#endif
+#ifdef BOARD_ESP32S3
+
+// SPI
+#define SPI_MOSI 15
+#define SPI_SCK 46
+#define SPI_CS 9
+#define SPI_DC 10
+#define SPI_RST 11
+#define SPI_BUSY 18
+
+// Current Sensor
+#define SENSE_CURRENT1 1
+#define SENSE_CURRENT2 2
+
+// MH-Z19
+#define MH_Z19_TX 4
+#define MH_Z19_RX 5
+
+// Servo Pins
+#define SERVO_PIN_0 45
+#define SERVO_PIN_1 48
+#define SERVO_PIN_2 47
+#define SERVO_PIN_3 21
+
+// LED Pins
+#define LED_PIN_1 4
+#define LED_PIN_2 5
+#define LED_PIN_3 19
+
+#endif  // BOARD_ESP32S3
+
+#endif  // ENV_HEADER
